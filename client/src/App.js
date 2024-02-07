a3946086271cd05386b89104d6a70ebfba0338ca
Hans Steffens <hans@steffens.co>
2024-02-07 10:14:26 -0500

    Fetch Spotify API Data with Credentials with Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import { useState, useEffect } from "react";
-import { accessToken, logout } from "./spotify";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
 import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
   }, []);
+
+  // Scroll to top of page when changing routes
+  // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
 
   return (
     <div className="App">
@@ -17,10 +44,37 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
-          </>
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+
+              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+
+              <Route path="/playlists" element={<h1>Playlists</h1>} />
+
+              <Route
+                path="/"
+                element={
+                  <>
+                    <button onClick={logout}>Log Out</button>
+
+                    {profile && (
+                      <div>
+                        <h1>{profile.display_name}</h1>
+                        <p>{profile.followers.total} Followers</p>
+                        {profile.images.length && profile.images[0].url && (
+                          <img src={profile.images[0].url} alt="Avatar" />
+                        )}
+                      </div>
+                    )}
+                  </>
+                }
+              />
+            </Routes>
+          </Router>
         )}
       </header>
     </div>
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Map for localStorage keys
 const LOCALSTORAGE_KEYS = {
@@ -124,3 +124,19 @@ const getAccessToken = () => {
 
 
 export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+/**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * @returns {Promise}
+ */
+export const getCurrentUserProfile = () => axios.get('/me');
+
