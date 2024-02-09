d1abc7f1e2d0a04ce88ae42c9f787c287373f82b
Hans Steffens <hans@steffens.co>
2024-02-09 12:43:12 -0500

    Get Playlists data and render the PlayListGrid component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,7 +6,12 @@ import {
   getTopArtists,
   getTopTracks,
 } from "../spotify";
-import { SectionWrapper, ArtistsGrid, TrackList } from "../components";
+import {
+  SectionWrapper,
+  ArtistsGrid,
+  TrackList,
+  PlaylistsGrid,
+} from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
@@ -68,7 +73,7 @@ const Profile = () => {
           </StyledHeader>
         </>
       )}
-      {topArtists && topTracks && (
+      {topArtists && topTracks && playlists && (
         <main>
           <SectionWrapper
             title="Top artists this month"
@@ -81,6 +86,9 @@ const Profile = () => {
             seeAllLink="/top-tracks"
           >
             <TrackList tracks={topTracks.items.slice(0, 10)} />
+          </SectionWrapper>
+          <SectionWrapper title="Playlists" seeAllLink="/playlists">
+            <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
           </SectionWrapper>
         </main>
       )}
